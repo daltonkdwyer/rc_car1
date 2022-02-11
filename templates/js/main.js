4cb2d61760b82351ca24cd2d4a4662a4d8e85c73
Dalton Dwyer <daltondwyer@MacBook-Pro-2.local>
2022-02-11 11:50:59 -0500

    cleaning up combination

--- templates/js/main.js
+++ templates/js/main.js
@@ -4,16 +4,6 @@ const mediaStreamConstraints = {
     video: true,
 }
 
-// // Initializes media stream 
-// navigator.mediaDevices.getUserMedia(mediaStreamConstraints).then(gotLocalMediaStream)
-
-// // Adds the video stream to webpage video element
-// function gotLocalMediaStream(mediaStream){
-//     document.querySelector('video').srcObject = mediaStream;
-// }
-
-// REWRITE BY YOU. GOOOD BOY!!!
-
 let constraints = {video: true, audio: false}
 
 async function getMedia(constraints) {
