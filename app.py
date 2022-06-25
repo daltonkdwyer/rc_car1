369e5f6454b264cb53e54b0efbe114706538aed9
Dalton Dwyer <daltondwyer@macbook-pro-4.mynetworksettings.com>
2022-06-25 16:14:42 -0400

    asaa

--- app.py
+++ app.py
@@ -10,40 +10,50 @@ PWM = Motor()
 
 @app.route('/', methods=['GET', 'POST'])
 def on():
-        if request.method == 'POST':
-            print("START OF PROCESS")
-            print("Request: " + str(request))
-        # You recieve a wierd immutable dict as your async FORM. So need to make it to normal dict below
-            dict_direction = request.form.to_dict()
-            direction = dict_direction['direction']
-            print("Direction: " + str(direction))
+    print("START OF PROCESS")
+    print("Request type: " + str(request.method))
 
-            if direction == 'FORWARD':
-                PWM.setMotorModel(0,0,0,0)
-                PWM.setMotorModel(2000,2000,2000,2000)
-                time.sleep(0.5)
-                PWM.setMotorModel(0,0,0,0)
 
-            if direction == 'BACK':
-                PWM.setMotorModel(0,0,0,0)
-                PWM.setMotorModel(-2000,-2000,-2000,-2000)
-                time.sleep(0.5)
-                PWM.setMotorModel(0,0,0,0)
+    if request.method == 'POST':
+        print("Request: " + str(request))
+    # You recieve a wierd immutable dict as your async FORM. So need to make it to normal dict below
+        dict_direction = request.form.to_dict()
+        direction = dict_direction['direction']
+        print("Direction: " + str(direction))
 
-            if direction == 'LEFT':
-                PWM.setMotorModel(0,0,0,0)
-                PWM.setMotorModel(0,0,2000,2000)
-                time.sleep(0.5)
-                PWM.setMotorModel(0,0,0,0)
-                
-            if direction == 'RIGHT':
-                PWM.setMotorModel(0,0,0,0)
-                PWM.setMotorModel(2000,2000,0,0)
-                time.sleep(0.5)
-                PWM.setMotorModel(0,0,0,0)
-            print("END OF PROCESS")
+        if direction == 'FORWARD':
+            PWM.setMotorModel(0,0,0,0)
+            PWM.setMotorModel(2000,2000,2000,2000)
+            time.sleep(0.5)
+            PWM.setMotorModel(0,0,0,0)
 
+        if direction == 'BACK':
+            PWM.setMotorModel(0,0,0,0)
+            PWM.setMotorModel(-2000,-2000,-2000,-2000)
+            time.sleep(0.5)
+            PWM.setMotorModel(0,0,0,0)
+
+        if direction == 'LEFT':
+            PWM.setMotorModel(0,0,0,0)
+            PWM.setMotorModel(0,0,2000,2000)
+            time.sleep(0.5)
+            PWM.setMotorModel(0,0,0,0)
+            
+        if direction == 'RIGHT':
+            PWM.setMotorModel(0,0,0,0)
+            PWM.setMotorModel(2000,2000,0,0)
+            time.sleep(0.5)
+            PWM.setMotorModel(0,0,0,0)
         return render_template('index.html')
+        print("END OF PROCESS")
 
+    if request.method == 'GET':
+        print("Request: " + str(request))
+        return render_template('index.html')
+        print("END OF PROCESS")
 
+    else:
+        return("NOT A GET OR POST REQUEST?")
+
+    print("FINAL END OF PROCESS")
 #test comment to check Git. Please ignore6
