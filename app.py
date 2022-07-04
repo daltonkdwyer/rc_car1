1ecccc58c2ed13a2794c2c8abd48cd6a196674a8
Dalton Dwyer <daltondwyer@macbook-pro-4.mynetworksettings.com>
2022-07-03 21:26:00 -0400

    cc

--- app.py
+++ app.py
@@ -36,20 +36,20 @@ def on():
         if direction == 'FORWARD':
             PWM.setMotorModel(0,0,0,0)
             PWM.setMotorModel(-2000,-2000,-2000,-2000)
-            time.sleep(0.5)
-            PWM.setMotorModel(0,0,0,0)
+            # time.sleep(0.5)
+            # PWM.setMotorModel(0,0,0,0)
 
         if direction == 'RIGHT':
             PWM.setMotorModel(0,0,0,0)
             PWM.setMotorModel(0,0,2000,2000)
-            time.sleep(0.5)
-            PWM.setMotorModel(0,0,0,0)
+            # time.sleep(0.5)
+            # PWM.setMotorModel(0,0,0,0)
             
         if direction == 'LEFT':
             PWM.setMotorModel(0,0,0,0)
             PWM.setMotorModel(2000,2000,0,0)
-            time.sleep(0.5)
-            PWM.setMotorModel(0,0,0,0)
+            # time.sleep(0.5)
+            # PWM.setMotorModel(0,0,0,0)
 
         request_num += 1
         print("END OF REQUEST")
