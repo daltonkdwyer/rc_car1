bc7cefb238ddb01b47794895a02b882c8f9f1fc1
daltonkdwyer <dalton.dwyer@gmail.com>
2023-04-13 19:12:52 -0700

    Final commit??

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, send
-# from Motor import *
+from Motor import *
 # from flask_ngrok import run_with_ngrok
 import time
 
@@ -42,33 +42,34 @@ def heartbeat():
 @socketio.on('move_command')
 def handle_my_custom_event(direction):
     print('Received Direction:', direction)
+    emit('server_message', direction)
 
-    # if direction == 'STOP':
-    #     PWM.setMotorModel(0,0,0,0)
+    if direction == 'STOP':
+        PWM.setMotorModel(0,0,0,0)
 
-    # if direction == 'BACK':
-    #     PWM.setMotorModel(0,0,0,0)
-    #     PWM.setMotorModel(2000,2000,2000,2000)
-    #     # time.sleep(0.5)
-    #     # PWM.setMotorModel(0,0,0,0)
+    if direction == 'BACK':
+        PWM.setMotorModel(0,0,0,0)
+        PWM.setMotorModel(2000,2000,2000,2000)
+        # time.sleep(0.5)
+        # PWM.setMotorModel(0,0,0,0)
 
-    # if direction == 'FORWARD':
-    #     PWM.setMotorModel(0,0,0,0)
-    #     PWM.setMotorModel(-2000,-2000,-2000,-2000)
-    #     # time.sleep(0.5)
-    #     # PWM.setMotorModel(0,0,0,0)
+    if direction == 'FORWARD':
+        PWM.setMotorModel(0,0,0,0)
+        PWM.setMotorModel(-2000,-2000,-2000,-2000)
+        # time.sleep(0.5)
+        # PWM.setMotorModel(0,0,0,0)
 
-    # if direction == 'LEFT':
-    #     PWM.setMotorModel(0,0,0,0)
-    #     PWM.setMotorModel(0,0,-2000,-2000)
-    #     # time.sleep(0.5)
-    #     # PWM.setMotorModel(0,0,0,0)
+    if direction == 'LEFT':
+        PWM.setMotorModel(0,0,0,0)
+        PWM.setMotorModel(0,0,-2000,-2000)
+        # time.sleep(0.5)
+        # PWM.setMotorModel(0,0,0,0)
         
-    # if direction == 'RIGHT':
-    #     PWM.setMotorModel(0,0,0,0)
-    #     PWM.setMotorModel(-2000,-2000,0,0)
-    #     # time.sleep(0.5)
-    #     # PWM.setMotorModel(0,0,0,0)
+    if direction == 'RIGHT':
+        PWM.setMotorModel(0,0,0,0)
+        PWM.setMotorModel(-2000,-2000,0,0)
+        # time.sleep(0.5)
+        # PWM.setMotorModel(0,0,0,0)
 
 if __name__ == '__main__':
     socketio.run(app, port=5000)
